e21b1a84f3864921b24e091c6af0549546244b8f
fajar2943 <fajar2943@gmail.com>
2020-09-03 23:13:50 +0700

    pengenalan router dan templating engine

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+ @extends('layout/main')
+ 
+ @section('title', 'Fajar Lintang Gumilang')
+
+ @section('container') 
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3">Belajar Laravel</h1>
+        </div>    
+    </div>    
+</div>
+@endsection
